11672ecbb1d3b6d2579572d75b41a5432e5c1281
vyko-24 <20223tn008[200~ajos>
2025-07-26 13:43:30 -0600

    Corrección de getters, setters y contructores en entidades

--- src/SERVER - CAMLA/camla/src/main/java/camila/camla/imagenes/Imagenes.java
+++ src/SERVER - CAMLA/camla/src/main/java/camila/camla/imagenes/Imagenes.java
@@ -10,9 +10,6 @@ import lombok.Setter;
 
 @Entity
 @Table(name ="imagenes")
-@NoArgsConstructor
-@Getter
-@Setter
 public class Imagenes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -67,5 +64,56 @@ public class Imagenes {
         this.url = url;
         this.usuario = usuario;
         this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Usuarios getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuarios usuario) {
+        this.usuario = usuario;
+    }
+
+    public Categorias getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categorias categoria) {
+        this.categoria = categoria;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Imagenes() {
     }
 }
