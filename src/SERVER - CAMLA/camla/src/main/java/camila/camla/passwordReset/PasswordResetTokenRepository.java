cdc6f5a89f6a6d5ffa0b384f78ec92c9a6f18f46
vyko-24 <20223tn008[200~ajos>
2025-08-17 11:08:10 -0600

    aer si jala

--- src/SERVER - CAMLA/camla/src/main/java/camila/camla/passwordReset/PasswordResetTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package camila.camla.passwordReset;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-    Optional<PasswordResetToken> findByToken(String token);
-}
